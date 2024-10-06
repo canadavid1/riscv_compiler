846928606361c37dac1fd513ab34f6ca143505e1
canadavid1 <davideikeland005@gmail.com>
2024-10-06 18:30:45 +0200

    even better codegen (now works with C extension)

--- advanced.py
+++ advanced.py
@@ -1,34 +1,14 @@
 
 from elftools.elf.elffile import ELFFile
 from elftools.elf.sections import SymbolTableSection
-import elftools.elf as elf
 import os
 import re
 from collections import defaultdict    
-
-def convert_line(line: str) -> str | None:
-    """ converts lines:
-    00000004 <_entry>:                              -> origin 0x00001 ; _entry:
-        4: 37 01 40 00   lui     sp, 0x400          -> 0x00400137   ; lui     sp, 0x400 
-        8: ef 00 80 00   jal     0x10 <main>        -> 0x008000ef   ; jal     0x10 <main>
-    """
-    if line == '' or line == '\n': return None
-    if (a:=re.match('([0-9a-fA-F]{8}) <(.*)>:',line.strip())):
-        addr = int(a[1],16)
-        sym = a[2]
-        return f"origin 0x{addr//4:05x} # {sym}:\t"
-    addr,i0,i1,i2,i3,instr = line.strip().split(None,5)
-    return f"0x{i3}{i2}{i1}{i0}\t# {addr} \t{instr}"
 
 def get_disasm_line(line: str) -> str | None:
     a = re.match(r'([ 0-9a-f]{8}):((?: [0-9a-f]{2})+) *\t(.+)',line)
     # if a:print(a[1],a[2],a[3],sep=" | ")
     return (int(a[1],16),('c.' if len(a[2])==6 else '')+a[3],len(a[2])//3) if a else (None,None,None)
-
-
-
-def disassemble_symbol(symbol: str,file: str) -> list[str]:
-    return os.popen(f"llvm-objdump --disassemble-symbols={symbol} {file}").readlines()[6:]
 
 def disassemble_section(section: str,file: str) -> list[str]:
     return os.popen(f"llvm-objdump -d -j {section} {file}").readlines()[4:]
@@ -51,7 +31,7 @@ if True:
         addr = sec.header.sh_addr
         data = sec.data()
         if addr:
-            symb[addr].append("S" + sec.name)
+            # symb[addr].append("S" + sec.name)
             if len(binout) < addr+len(data):
                 binout.extend([0]*(addr+len(data)-len(binout)))
             for i,v in enumerate(sec.data()):
@@ -67,30 +47,23 @@ if True:
     for i in range(1,symtab.num_symbols()):
         sym = symtab.get_symbol(i)
         if sym.name and sym.name[0] != '$' and sym.entry.st_size > 0:
-            # print(f"{sym.name:20}",sym.entry)
             symb[sym.entry['st_value']].append(sym.name)
-    # for i,v in enumerate(binout):
-    #     if i in symb:
-    #         # print("\n",f"{i:3x}",*symb[i],sep=" ",end=":\n\t")
-    #     print(f"{v:02x}",end=" ")
-    #     if i in edisasm:
-    #         # print("\t",disasm[i],end="\n\t")
-    # print()
 
 def emit(binout: bytearray,disasm: dict[int,str],symb: dict[int,list[str]]):
     for i in range(4,len(binout),4):
         ass = []
+        out = False
+        v = int.from_bytes(binout[i:i+4],'little')
         for j in range(i,i+4):
             for k in symb[j]:
-                yield f"#{j:4x}   {k}:" if j == i else f"#{i:4x}{j-i:+1} {k}:"
+                yield f"#{j:4x}   {k}:"
             if j in disasm:
-                ass.append(disasm[j])
-        v = int.from_bytes(binout[i:i+4],'little')
-        if not ass:
+                if not out:
+                    yield f"0x{v:08x} #{j:3x} {disasm[j]}"
+                    out = True
+                else:
+                    yield f"           #{j:3x} {disasm[j]}"
+        if not out:
             yield f"0x{v:08x}"
-            continue
-        yield f"0x{v:08x} #{i:3x} {ass[0]}"
-        for a in ass[1:]:
-            yield f"           #    {a}"
 
 print(*emit(binout,disasm,symb),sep="\n")
